e87c350d9a5b4f3b1b99e003727a06d00bac3c21
demirdogukan <dogukan.demir@yahoo.com>
2022-10-09 23:42:15 +0300

    pygame-setup

---
+++ Scripts/main.py
@@ -0,0 +1,22 @@
+from settings import *
+import pygame, sys
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+    
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
