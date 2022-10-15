d24676e57d86db3e9ce34a3eebbd2ab073bbf039
demirdogukan <dogukan.demir@yahoo.com>
2022-10-15 23:40:25 +0300

    added change tool

--- Scripts/player.py
+++ Scripts/player.py
@@ -23,11 +23,14 @@ class Player(pg.sprite.Sprite):
 
         # timers
         self.timers: dict = {
-            'tool use': Timer(350, self.use_tool)
+            'tool use': Timer(350, self.use_tool),
+            'tool switch': Timer(200)
         }
 
         # tools
-        self.selected_tool = 'axe'
+        self.tools = ['hoe', 'axe', 'water']
+        self.tool_index = 0
+        self.selected_tool = self.tools[self.tool_index]
 
     def use_tool(self):
         print(self.selected_tool)
@@ -71,6 +74,17 @@ class Player(pg.sprite.Sprite):
                 self.timers['tool use'].activate()
                 self.direction = pg.math.Vector2()
                 self.frame_index = 0
+
+            # change tool
+            if keys[pg.K_q] and not self.timers['tool switch'].active:
+                self.timers['tool switch'].activate()
+
+                self.tool_index += 1
+                self.tool_index = self.tool_index if self.tool_index < len(self.tools) else 0
+
+                self.selected_tool = self.tools[self.tool_index]
+
+            
 
     def update_timers(self):
         for timer in self.timers.values():
